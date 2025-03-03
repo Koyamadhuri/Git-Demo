edcc4c02ae9dec6a20650cbd1b7c6f14faa633df
Madhuri <koyamadhuri1234@gmail.com>
2025-03-03 13:38:49 +0530

    New commit for python code.

--- Example1.py
+++ Example1.py
@@ -2,5 +2,7 @@ num=int(input('enter a number:'))
 num1=int(input('enter a another number:'))
 sum=num+num1
 diff=num-num1
+mult=num*num1
 print('Difference of two numbers:',diff)
 print('Addition of two numbers:',sum)
+print('multiply the two numbers:',mult)
