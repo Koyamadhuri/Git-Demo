b9af77905a14ae6f6712bd1bd83964c17df3f9af
Madhuri <koyamadhuri1234@gmail.com>
2025-03-03 12:41:34 +0530

    This is my first commit

---
+++ Example1.py
@@ -0,0 +1,6 @@
+num=int(input('enter a number:'))
+num1=int(input('enter a another number:'))
+sum=num+num1
+diff=num-num1
+print('Difference of two numbers:',diff)
+print('Addition of two numbers:',sum)
